c9ea8dde375ab28d46125d063485dff9f1831b4d
Carlos <crrlos1801@gmail.com>
2015-10-14 16:40:36 -0600

    agregado TabLayout para las opciones entrar y registro

--- app/src/main/java/com/mychess/mychess/Acceso.java
+++ app/src/main/java/com/mychess/mychess/Acceso.java
@@ -1,13 +1,18 @@
 package com.mychess.mychess;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
+import android.widget.TableLayout;
 
 public class Acceso extends AppCompatActivity {
 
@@ -17,7 +22,7 @@ public class Acceso extends AppCompatActivity {
         setContentView(R.layout.activity_acceso);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        setTabs();
     }
 
     @Override
@@ -40,5 +45,35 @@ public class Acceso extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setTabs(){
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Entrar"));
+        tabLayout.addTab(tabLayout.newTab().setText("Registrarse"));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
