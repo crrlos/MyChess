084b65268ffe83f8d3b4e62c3582c000a62c5d41
Carlos <crrlos1801@gmail.com>
2015-10-15 10:23:21 -0600

    agregado el evento onclick del botón entrar

--- app/src/main/java/com/mychess/mychess/Acceso.java
+++ app/src/main/java/com/mychess/mychess/Acceso.java
@@ -1,5 +1,6 @@
 package com.mychess.mychess;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -8,9 +9,12 @@ import android.support.v7.widget.Toolbar;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class Acceso extends AppCompatActivity {
+    Button entrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +64,15 @@ public class Acceso extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
-
+                entrar = (Button) findViewById(R.id.button);
+                entrar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Acceso.this,Juego.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
             }
 
             @Override
