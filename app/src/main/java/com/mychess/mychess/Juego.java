1cd89c7be6d89f256fdbd10ac64381c1cea20955
Carlos <crrlos1801@gmail.com>
2015-10-15 11:19:43 -0600

    modificación del navigation drawer de la activity del juego

--- app/src/main/java/com/mychess/mychess/Juego.java
+++ app/src/main/java/com/mychess/mychess/Juego.java
@@ -87,17 +87,11 @@ public class Juego extends AppCompatActivity implements NavigationView.OnNavigat
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
+        if (id == R.id.jugar) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.amigos) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.logout) {
 
         }
 
