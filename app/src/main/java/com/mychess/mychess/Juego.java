89947ab9fe6155fbb9284ae99386f19758590787
Carlos <crrlos1801@gmail.com>
2015-10-15 11:07:26 -0600

    removida instrucción dentro del onclicklistener del floating button

--- app/src/main/java/com/mychess/mychess/Juego.java
+++ app/src/main/java/com/mychess/mychess/Juego.java
@@ -35,8 +35,7 @@ public class Juego extends AppCompatActivity implements NavigationView.OnNavigat
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
 
