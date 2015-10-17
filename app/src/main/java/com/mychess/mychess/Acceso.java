49343b2ef5ab79414ec9974b6f295444a533eed5
Carlos <crrlos1801@gmail.com>
2015-10-16 22:07:59 -0600

    reemplazado el id el menu

--- app/src/main/java/com/mychess/mychess/Acceso.java
+++ app/src/main/java/com/mychess/mychess/Acceso.java
@@ -41,7 +41,7 @@ public class Acceso extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.rendir) {
             return true;
         }
 
