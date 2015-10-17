3eebb1468c7d8480065c419fc8accad26f7fdec8
Carlos <crrlos1801@gmail.com>
2015-10-16 22:41:38 -0600

    eliminado el floating actioni button y agrega la función onCreateOptionsMenu

--- app/src/main/java/com/mychess/mychess/Amigos.java
+++ app/src/main/java/com/mychess/mychess/Amigos.java
@@ -6,12 +6,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class Amigos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amigos);
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -25,15 +28,13 @@ public class Amigos extends AppCompatActivity {
              finish();
             }
         });
-       
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_amigos, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
