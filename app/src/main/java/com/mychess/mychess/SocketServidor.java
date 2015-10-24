8e0798d0891798b927fa95e11b20c4c89a6c3386
Carlos <crrlos1801@gmail.com>
2015-10-23 22:58:35 -0600

    agregado subproceso para conectar al socket

--- app/src/main/java/com/mychess/mychess/SocketServidor.java
+++ app/src/main/java/com/mychess/mychess/SocketServidor.java
@@ -1,5 +1,7 @@
 package com.mychess.mychess;
 
+import android.os.AsyncTask;
+
 import java.io.IOException;
 import java.net.Socket;
 
@@ -9,17 +11,29 @@ class SocketServidor {
     private static  Socket socket;
 
 
-    public static void conectar(){
+    public  void conectar(){
 
-        try {
-            socket = new Socket("servermychess.tk",40000);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+       Conectar conectar  = new Conectar();
+        conectar.execute();
 
     }
 
-    public static Socket getSocket() {
+    public  Socket getSocket() {
         return socket;
+    }
+
+    class Conectar extends AsyncTask<Void,Integer,Boolean>{
+
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try{
+                socket = new Socket("servermychess.tk",40000);
+
+            }catch(Exception ex){
+
+            }
+            return null;
+        }
     }
 }
