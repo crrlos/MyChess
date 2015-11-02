b3b92aea8e3f4250b4e26340b5665972b921e6f5
Carlos <crrlos1801@gmail.com>
2015-11-01 20:24:24 -0600

    quitados los tres puntos de la barra de menú

--- app/src/main/java/com/mychess/mychess/Acceso.java
+++ app/src/main/java/com/mychess/mychess/Acceso.java
@@ -30,7 +30,7 @@ public class Acceso extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_acceso, menu);
-        return true;
+        return false;
     }
 
     @Override
@@ -87,4 +87,5 @@ public class Acceso extends AppCompatActivity {
             }
         });
     }
+
 }
