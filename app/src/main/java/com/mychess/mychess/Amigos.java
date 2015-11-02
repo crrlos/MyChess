6e83a62fbd3cc0aaa356a7b662429d2886f3b8ae
Carlos <crrlos1801@gmail.com>
2015-11-02 12:35:32 -0600

    agregada activity agregar amigos

--- app/src/main/java/com/mychess/mychess/Amigos.java
+++ app/src/main/java/com/mychess/mychess/Amigos.java
@@ -1,6 +1,6 @@
 package com.mychess.mychess;
 
-import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,33 +8,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class Amigos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amigos);
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        toolbar.setNavigationIcon(R.drawable.oie_transparent);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-             finish();
-            }
-        });
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_amigos, menu);
+        inflater.inflate(R.menu.menu_amigos,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id  = item.getItemId();
+        switch (id){
+            case R.id.agregar:
+            {
+                Intent intent = new Intent(Amigos.this,AgregarAmigos.class);
+                startActivity(intent);
+            }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
