1e4fd1c254320e35011b981effc5768f670709ef
Carlos <crrlos1801@gmail.com>
2015-11-04 10:51:51 -0600

    agregado evento onClickListener al botón entrar

--- app/src/main/java/com/mychess/mychess/LoginFragment.java
+++ app/src/main/java/com/mychess/mychess/LoginFragment.java
@@ -5,11 +5,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class LoginFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.login_fragment, container, false);
+        View view = inflater.inflate(R.layout.login_fragment, container, false);
+        Button entrar = (Button) view.findViewById(R.id.button);
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return view;
     }
 }
