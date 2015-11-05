c68215e4f082ca18fb6daa05e7f85c87c88f2552
Carlos <crrlos1801@gmail.com>
2015-11-05 13:22:11 -0600

    agregado un stream de salida para enviar el nombre del usuario al servidor

--- app/src/main/java/com/mychess/mychess/SocketServidor.java
+++ app/src/main/java/com/mychess/mychess/SocketServidor.java
@@ -10,35 +10,35 @@ import java.net.Socket;
 
 class SocketServidor {
 
-    private static  Socket socket;
+    private static Socket socket;
 
-    SocketServidor(){
+    SocketServidor() {
         socket = new Socket();
     }
 
 
-    public  void conectar(){
+    public void conectar() {
 
-       Conectar conectar  = new Conectar();
+        Conectar conectar = new Conectar();
         conectar.execute();
 
     }
 
-    public static  Socket getSocket() {
+    public static Socket getSocket() {
         return socket;
     }
 
-    class Conectar extends AsyncTask<Void,Integer,Boolean>{
-
+    class Conectar extends AsyncTask<Void, Integer, Boolean> {
+        DataOutputStream out;
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            try{
-                socket = new Socket("servermychess.tk",40000);
+            try {
+                socket = new Socket("servermychess.tk", 40000);
+                out = new DataOutputStream(socket.getOutputStream());
+                out.writeUTF(Usuario.getUsuario());
 
-
-
-            }catch(Exception ex){
+            } catch (Exception ex) {
 
             }
             return null;
