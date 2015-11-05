c62bb92ffa34e21a3e53616b177478f14f3ffbb9
Carlos <crrlos1801@gmail.com>
2015-11-05 14:30:41 -0600

    agregada clase RecibirInvitacion

---
+++ app/src/main/java/com/mychess/mychess/Chess/RecibirInvitacion.java
@@ -0,0 +1,13 @@
+package com.mychess.mychess.Chess;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Carlos on 05/11/2015.
+ */
+public class RecibirInvitacion extends AsyncTask<Void,Void,Void> {
+    @Override
+    protected Void doInBackground(Void... params) {
+        return null;
+    }
+}
