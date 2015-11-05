d305dbf2625272704991cf65c1f7434273375783
Carlos <crrlos1801@gmail.com>
2015-11-05 12:55:18 -0600

    Agregadas funciones para el manejo de la SharedPreference del usuario

--- app/src/main/java/com/mychess/mychess/Usuario.java
+++ app/src/main/java/com/mychess/mychess/Usuario.java
@@ -1,7 +1,42 @@
 package com.mychess.mychess;
 
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by Carlos on 05/11/2015.
  */
 public class Usuario {
+    SharedPreferences preferences;
+    Context context;
+
+    Usuario(Context context) {
+        this.context = context;
+        preferences = context.getSharedPreferences("usuario", context.MODE_PRIVATE);
+    }
+
+    public boolean isEmpty() {
+        if(preferences.getString("usuario",null) == null)
+            return true;
+        return false;
+    }
+
+    public String getUsuario() {
+        return preferences.getString("usuario", null);
+    }
+
+    public void setUsuario(String usuario) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("usuario", usuario);
+        editor.commit();
+    }
+
+    public void clear() {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.commit();
+    }
+
+
 }
