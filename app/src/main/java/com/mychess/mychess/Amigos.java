f6227106da22b230822d6f9b2970d45fd88a5c82
Carlos <crrlos1801@gmail.com>
2015-11-10 11:27:55 -0600

    Agregada clase EsperarRespuesta y progressBar para esperar la respuesta  a la invitación

--- app/src/main/java/com/mychess/mychess/Amigos.java
+++ app/src/main/java/com/mychess/mychess/Amigos.java
@@ -1,6 +1,9 @@
 package com.mychess.mychess;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,8 +13,18 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class Amigos extends AppCompatActivity {
+    Button invitar;
+    EditText amigo;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,19 @@ public class Amigos extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        invitar = (Button) findViewById(R.id.btnInvitar);
+        amigo = (EditText) findViewById(R.id.txtAmigo);
+
+        invitar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (invitarAmigo()) {
+                    EsperarRespuesta respuesta = new EsperarRespuesta();
+                    respuesta.execute();
+
+                }
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
@@ -27,21 +53,75 @@ public class Amigos extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_amigos,menu);
+        inflater.inflate(R.menu.menu_amigos, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id  = item.getItemId();
-        switch (id){
-            case R.id.agregar:
-            {
-                Intent intent = new Intent(Amigos.this,AgregarAmigos.class);
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.agregar: {
+                Intent intent = new Intent(Amigos.this, AgregarAmigos.class);
                 startActivity(intent);
             }
-                break;
+            break;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private boolean invitarAmigo() {
+        try {
+            DataOutputStream out = new DataOutputStream(SocketServidor.getSocket().getOutputStream());
+            out.writeInt(1);
+            out.writeUTF(amigo.getText().toString());
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+
+    }
+
+    class EsperarRespuesta extends AsyncTask<Void,Integer,Void>{
+            boolean continuar = true;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            while(continuar){
+                try {
+                    DataInputStream in = new DataInputStream(SocketServidor.getSocket().getInputStream());
+                    publishProgress(in.readInt());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            if(values[0] == 1){
+                progressDialog.cancel();
+                Toast.makeText(Amigos.this, "invitación aceptada", Toast.LENGTH_SHORT).show();
+            }else
+            {
+                progressDialog.cancel();
+                Toast.makeText(Amigos.this, "invitación rechazada", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(Amigos.this);
+            progressDialog.setMessage("Por favor espere...");
+            progressDialog.setTitle("Esperando respuesta");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+    }
+
 }
