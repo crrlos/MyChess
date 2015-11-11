259067d7fb2a59941d03801466f595a1e8766597
Carlos <crrlos1801@gmail.com>
2015-11-11 10:33:48 -0600

    Agregadas variables para esperar respuesta y recibir movimientos

--- app/src/main/java/com/mychess/mychess/ThreadsData.java
+++ app/src/main/java/com/mychess/mychess/ThreadsData.java
@@ -4,8 +4,21 @@ package com.mychess.mychess;
  * Created by Carlos on 10/11/2015.
  */
 public class ThreadsData {
+
+    /**variables para el hilo Recibir Invitación*/
     public static boolean RECIBIR_INVITACION = false;
     public static String  INVITACION_USUARIO = "";
+
+    /**Variables para el hilo Esperar Confirmación**/
+
+    public static boolean ESPERAR_RESPUESTA = false;
+    public static int     RESPUESTA;
+
+    /***Variables para el hilo Recibir Movimientos*/
+
+    public static boolean RECIBIR_MOVIMIENTO = false;
+    public static String  MOVIMIENTO = "";
+
 
 
 
