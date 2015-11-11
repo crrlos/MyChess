4ba4ae395b78f6afce0cc18647d630f3bbd87cae
Carlos <crrlos1801@gmail.com>
2015-11-11 09:41:38 -0600

    Cambio de la clase EsperarRespuesta para usar la clase Thread

--- app/src/main/java/com/mychess/mychess/Amigos.java
+++ app/src/main/java/com/mychess/mychess/Amigos.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -25,6 +26,7 @@ public class Amigos extends AppCompatActivity {
     Button invitar;
     EditText amigo;
     ProgressDialog progressDialog;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +38,17 @@ public class Amigos extends AppCompatActivity {
         invitar = (Button) findViewById(R.id.btnInvitar);
         amigo = (EditText) findViewById(R.id.txtAmigo);
 
+
+        handler = new Handler();
+
         invitar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (invitarAmigo()) {
+                    iniciarDialogo();
                     EsperarRespuesta respuesta = new EsperarRespuesta();
-                    respuesta.execute();
+                    respuesta.start();
+
 
                 }
             }
@@ -82,46 +89,51 @@ public class Amigos extends AppCompatActivity {
 
     }
 
-    class EsperarRespuesta extends AsyncTask<Void,Integer,Void>{
-            boolean continuar = true;
+    class EsperarRespuesta  extends Thread {
+        boolean continuar = true;
 
         @Override
-        protected Void doInBackground(Void... params) {
-            while(continuar){
+        public void run() {
+            super.run();
+            while (continuar) {
                 try {
-                    DataInputStream in = new DataInputStream(SocketServidor.getSocket().getInputStream());
-                    publishProgress(in.readInt());
-
-                } catch (IOException e) {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
 
-            }
-            return null;
-        }
+                if (ThreadsData.ESPERAR_RESPUESTA) {
+                    ThreadsData.ESPERAR_RESPUESTA = false;
+                    handler.post(run);
 
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-            if(values[0] == 1){
-                progressDialog.cancel();
-                Toast.makeText(Amigos.this, "invitación aceptada", Toast.LENGTH_SHORT).show();
-            }else
-            {
-                progressDialog.cancel();
-                Toast.makeText(Amigos.this, "invitación rechazada", Toast.LENGTH_SHORT).show();
+                }
             }
         }
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressDialog = new ProgressDialog(Amigos.this);
-            progressDialog.setMessage("Por favor espere...");
-            progressDialog.setTitle("Esperando respuesta");
-            progressDialog.setCancelable(false);
-            progressDialog.show();
-        }
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                if (ThreadsData.RESPUESTA == 1) {
+                    progressDialog.cancel();
+                    Intent intent = new Intent(Amigos.this, Juego.class);
+                    intent.putExtra("juegoIniciado", true);
+                    startActivity(intent);
+                } else {
+                    progressDialog.cancel();
+                    Toast.makeText(Amigos.this, "invitación rechazada", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+
+    }
+
+    private void iniciarDialogo(){
+        progressDialog = new ProgressDialog(Amigos.this);
+        progressDialog.setMessage("Por favor espere...");
+        progressDialog.setTitle("Esperando respuesta");
+        progressDialog.setCancelable(true);
+        progressDialog.show();
     }
 
 }
