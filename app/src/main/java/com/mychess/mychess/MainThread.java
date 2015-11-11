63e80e2a3a580cc4c3dfe6974736a9628dcf1aad
Carlos <crrlos1801@gmail.com>
2015-11-11 07:01:50 -0600

    Clase que administrará todas las respuestas del servidor

---
+++ app/src/main/java/com/mychess/mychess/MainThread.java
@@ -0,0 +1,53 @@
+package com.mychess.mychess;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+/**
+ * Created by Carlos on 10/11/2015.
+ */
+public class MainThread extends Thread{
+    Context context;
+    boolean continuar = true;
+    DataInputStream in;
+
+
+
+    @Override
+    public void run() {
+        super.run();
+        while(continuar){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                in = new DataInputStream(SocketServidor.getSocket().getInputStream());
+                int op = in.readInt();
+                switch (op)
+                {
+                    case 1:{
+                        ThreadsData.RECIBIR_INVITACION = true;
+                        in = new DataInputStream(SocketServidor.getSocket().getInputStream());
+                        ThreadsData.INVITACION_USUARIO = in.readUTF();
+
+                    }
+
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+
+
+}
