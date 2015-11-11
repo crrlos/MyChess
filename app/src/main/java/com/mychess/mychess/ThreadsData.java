dd676a097f77badb6bf0c230a20cf0862f05b37e
Carlos <crrlos1801@gmail.com>
2015-11-11 07:02:29 -0600

    Clase que contiene variables públicas que serán leidas por sus correspondientes hilos

---
+++ app/src/main/java/com/mychess/mychess/ThreadsData.java
@@ -0,0 +1,12 @@
+package com.mychess.mychess;
+
+/**
+ * Created by Carlos on 10/11/2015.
+ */
+public class ThreadsData {
+    public static boolean RECIBIR_INVITACION = false;
+    public static String  INVITACION_USUARIO = "";
+
+
+
+}
