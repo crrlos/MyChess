71e551cd5e0f39a3c1fc02b4652b6ba817d291e2
Carlos <crrlos1801@gmail.com>
2015-11-11 07:13:44 -0600

    Se eliminó la creación de una nueva instancia de RecibirInvitación

--- app/src/main/java/com/mychess/mychess/Juego.java
+++ app/src/main/java/com/mychess/mychess/Juego.java
@@ -574,7 +574,7 @@ public class Juego extends AppCompatActivity implements NavigationView.OnNavigat
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     enviarRespuesta(0,ThreadsData.INVITACION_USUARIO);
-                   new RecibirInvitacion().execute();
+
                     dialog.cancel();
                 }
             };
